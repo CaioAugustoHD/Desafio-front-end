45a3cd30c4cf871816fad13ff4bbb709ebe172f9
CaioAugustoHD <caioaugustosbs@gmail.com>
2023-03-15 05:32:20 -0300

    feat: Elementos no componente Shopping

---
+++ src/components/Shopping/ProductsTable/index.tsx
@@ -0,0 +1,34 @@
+export function ProductsTable() {
+    return (
+        <table className="table">
+            <thead>
+                <tr>
+                    <th scope="col">Produto</th>
+                    <th scope="col">Valor</th>
+                    <th scope="col">Quantidade</th>
+                    <th scope="col">Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td scope="row">
+                        <p>Hamburguer</p>
+                        <p>Sem pão</p>
+                    </td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <td scope="row">
+                        <p>Hamburguer</p>
+                        <p>Sem pão</p>
+                    </td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
---
+++ src/components/Shopping/Saldo/index.tsx
@@ -0,0 +1,14 @@
+export function Saldo() {
+    return (
+        <section className="">
+            <div>
+                <span>Total</span>
+                <span>R$ 40,00</span>
+            </div>
+            <div>
+                <button className="btn btn-primary">V Finalizar</button>
+                <button className="btn btn-outline-primary">X Cancelar</button>
+            </div>
+        </section>
+    )
+}
