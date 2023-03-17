24d9ae981df25d0fd64f6038fe2ea414ac38564f
CaioAugustoHD <caioaugustosbs@gmail.com>
2023-03-17 05:45:04 -0300

    feat: Tabela para todas as categorias de produtos

--- src/components/modals/ProductModal/Table/index.tsx
+++ src/components/modals/ProductModal/Table/index.tsx
@@ -1,7 +1,18 @@
 import { Counter } from '../../../Counter'
 import './style.css'
 
-export function Table() {
+interface TableProps {
+    category_id: number,
+    price: number
+}
+
+export function Table(props: TableProps) {
+
+    const formattedPrice = props.price.toLocaleString('pt-br',{
+        style: 'currency',
+        currency: 'BRL'
+    })
+
     return (
         <table className="table table-borderless table-response modalTable mt-3">
             <thead>
@@ -11,28 +22,145 @@ export function Table() {
                     <th scope="col">Quantidade</th>
                 </tr>
             </thead>
-            <tbody>
+            {props.category_id == 1 ? <tbody>
                 <tr>
-                    <td scope="row">Calabresa</td>
-                    <td>R$ 40,00</td>
+                    <td scope="row">Hambúrguer tradicional</td>
+                    <td>{formattedPrice}</td>
                     <td><Counter/></td>
                 </tr>
                 <tr>
-                    <td scope="row">Sensação</td>
-                    <td>R$ 40,00</td>
+                    <td scope="row">Hambúrguer artesanal</td>
+                    <td>{formattedPrice}</td>
                     <td><Counter/></td>
                 </tr>
                 <tr>
-                    <td scope="row">Frango c/catupiry</td>
-                    <td>R$ 40,00</td>
+                    <td scope="row">Hambúrguer vegetariano</td>
+                    <td>{formattedPrice}</td>
                     <td><Counter/></td>
                 </tr>
                 <tr>
-                    <td scope="row">Calabresa c/bacon</td>
-                    <td>R$ 40,00</td>
+                    <td scope="row">Hambúrguer vegano</td>
+                    <td>{formattedPrice}</td>
                     <td><Counter/></td>
                 </tr>
             </tbody>
+
+            : props.category_id == 2 ? <tbody>
+            <tr>
+                <td scope="row">Coca c/gelo</td>
+                <td>{formattedPrice}</td>
+                <td><Counter/></td>
+            </tr>
+            <tr>
+                <td scope="row">Coca sem gelo</td>
+                <td>{formattedPrice}</td>
+                <td><Counter/></td>
+            </tr>
+            <tr>
+                <td scope="row">Coca zero c/gelo</td>
+                <td>{formattedPrice}</td>
+                <td><Counter/></td>
+            </tr>
+            <tr>
+                <td scope="row">Coca zero sem gelo</td>
+                <td>{formattedPrice}</td>
+                <td><Counter/></td>
+            </tr>
+        </tbody>
+
+        : props.category_id == 3 ? <tbody>
+            <tr>
+                <td scope="row">Calabresa</td>
+                <td>{formattedPrice}</td>
+                <td><Counter/></td>
+            </tr>
+            <tr>
+                <td scope="row">Sensação</td>
+                <td>{formattedPrice}</td>
+                <td><Counter/></td>
+            </tr>
+            <tr>
+                <td scope="row">Frango c/catupiry</td>
+                <td>{formattedPrice}</td>
+                <td><Counter/></td>
+            </tr>
+            <tr>
+                <td scope="row">Calabresa c/bacon</td>
+                <td>{formattedPrice}</td>
+                <td><Counter/></td>
+            </tr>
+        </tbody>
+
+        : props.category_id == 4 ? <tbody>
+            <tr>
+                <td scope="row">Hambúrguer tradicional</td>
+                <td>{formattedPrice}</td>
+                <td><Counter/></td>
+            </tr>
+            <tr>
+                <td scope="row">Hambúrguer artesanal</td>
+                <td>{formattedPrice}</td>
+                <td><Counter/></td>
+            </tr>
+            <tr>
+                <td scope="row">Hambúrguer vegetariano</td>
+                <td>{formattedPrice}</td>
+                <td><Counter/></td>
+            </tr>
+            <tr>
+                <td scope="row">Hambúrguer vegano</td>
+                <td>{formattedPrice}</td>
+                <td><Counter/></td>
+            </tr>
+        </tbody>
+
+        : props.category_id == 5 ? <tbody>
+            <tr>
+                <td scope="row">Laranja</td>
+                <td>{formattedPrice}</td>
+                <td><Counter/></td>
+            </tr>
+            <tr>
+                <td scope="row">Abacaxi</td>
+                <td>{formattedPrice}</td>
+                <td><Counter/></td>
+            </tr>
+            <tr>
+                <td scope="row">Morango</td>
+                <td>{formattedPrice}</td>
+                <td><Counter/></td>
+            </tr>
+            <tr>
+                <td scope="row">Manga</td>
+                <td>{formattedPrice}</td>
+                <td><Counter/></td>
+            </tr>
+        </tbody>
+
+        : props.category_id == 6 ? <tbody>
+            <tr>
+                <td scope="row">Açaí pura</td>
+                <td>{formattedPrice}</td>
+                <td><Counter/></td>
+            </tr>
+            <tr>
+                <td scope="row">Açaí com frutas</td>
+                <td>{formattedPrice}</td>
+                <td><Counter/></td>
+            </tr>
+            <tr>
+                <td scope="row">Açaí com doces</td>
+                <td>{formattedPrice}</td>
+                <td><Counter/></td>
+            </tr>
+            <tr>
+                <td scope="row">Açaí com frutas e doces</td>
+                <td>{formattedPrice}</td>
+                <td><Counter/></td>
+            </tr>
+        </tbody>
+        : null
+            }
         </table>
     )
 }
