ea49b2f40584df554287316c0793a8f12a64fd4b
CaioAugustoHD <caioaugustosbs@gmail.com>
2023-03-18 07:05:03 -0300

    fix: Correções nas funções e na tipagem do Context

--- src/components/contexts/CartContext.tsx
+++ src/components/contexts/CartContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from "react";
+import React, { createContext, ReactNode, useState } from "react";
 
 
 interface ProductType {
@@ -12,25 +12,25 @@ interface ProductType {
 
 
 interface defaultType {
-    addItemsToCart(): any
-    removeItemsToCart(): any
-    cartProducts: []
+    addItemsToCart(selectedProduct: ProductType): void,
+    removeItemsToCart(selectedProduct: ProductType): void,
+    cartProducts: ProductType[]
 }
 
-const defaultValue = {
+const DEFAULT_VALUE = {
     addItemsToCart: () => {},
     removeItemsToCart: () => {},
     cartProducts: []
 }
 
-export const CartContext = createContext<defaultType>({defaultValue})
+export const CartContext = createContext<defaultType>(DEFAULT_VALUE)
 
 export function CartProvider({ children }: {children: ReactNode}) {
-    const [cartProducts, setCartProducts] = useState<ProductType[]>([])
+    const [cartProducts, setCartProducts] = useState<ProductType[]>(DEFAULT_VALUE.cartProducts)
 
     function addItemsToCart(selectedProduct: ProductType) {
         const copyCartProducts = [...cartProducts]
-        const item = copyCartProducts.find(product => product.orderId == selectedProduct.orderId)
+        const item = copyCartProducts.find(product => product.orderId == selectedProduct.orderId && product.id === selectedProduct.id)
 
         item ? item.quantity = item.quantity + 1
         : copyCartProducts.push({ id: selectedProduct.id, name: selectedProduct.name, price: selectedProduct.price, details: selectedProduct.details, orderId: selectedProduct.orderId, quantity: 1})
@@ -40,13 +40,13 @@ export function CartProvider({ children }: {children: ReactNode}) {
 
     function removeItemsToCart(selectedProduct: ProductType) {
         const copyCartProducts = [...cartProducts]
-        const item = copyCartProducts.find(product => product.orderId == selectedProduct.orderId)
+        const item = copyCartProducts.find(product => product.orderId == selectedProduct.orderId && product.id === selectedProduct.id)
 
         if(item && item.quantity > 1) {
             item.quantity = item.quantity - 1
             setCartProducts(copyCartProducts)
         } else {
-            const newCopyCartProducts = copyCartProducts.filter(product => product.orderId !== selectedProduct.orderId)
+            const newCopyCartProducts = copyCartProducts.filter(product => product.orderId !== selectedProduct.orderId || product.id !== selectedProduct.id)
             setCartProducts(newCopyCartProducts)
         }
     }
