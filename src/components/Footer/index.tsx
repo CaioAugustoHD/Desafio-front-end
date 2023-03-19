7cfaea25085250597f2eb48a24bc18dad3a65db2
CaioAugustoHD <caioaugustosbs@gmail.com>
2023-03-19 14:42:38 -0300

    feat: Finalização do componente Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,16 @@
+import profilePic from "../../assets/profilePic.png"
+
 export function Footer() {
     const footerStyle = {
-        height: "85px"
+        height: "85px",
+        boxShadow: "0 2.6px 7.25px 0 #00000014",
+        color: "#696969"
     }
 
     return (
         <footer className="container-fluid bg-white d-flex align-items-center" style={footerStyle}>
-            <span className="m-5">Caio Augusto</span>
+            <img className="img-fluid rounded-circle ms-1 ms-md-4" src={profilePic} alt="Profile Pic" style={{height: "53px"}} />
+            <span className="my-5 ms-4">Amanda Morais</span>
         </footer>
     )
 }
