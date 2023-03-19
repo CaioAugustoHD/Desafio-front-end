3a2c844a23c309cbe250abb066304b1eb9c9e94c
CaioAugustoHD <caioaugustosbs@gmail.com>
2023-03-19 17:27:17 -0300

    feat: Estilização no scrollbar

--- src/components/Shopping/index.tsx
+++ src/components/Shopping/index.tsx
@@ -11,10 +11,10 @@ export function Shopping() {
 
     return (
         <>
-            <div className="offcanvas-lg offcanvas-end  h-100" tabindex="-1" id="offcanvasResponsive" aria-labelledby="offcanvasResponsiveLabel">
+            <div className="offcanvas-lg offcanvas-end  h-100 p-0" tabindex="-1" id="offcanvasResponsive" aria-labelledby="offcanvasResponsiveLabel">
                 <div className="offcanvas-header d-block ms-4" style={offcanvasHeaderStyle}>
                 </div>            
-                <div className="offcanvas-body d-flex flex-column pt-4 align-items-center overflow-auto" style={{height: "calc(100% - 91px)"}}>
+                <div className="offcanvas-body d-flex flex-column pt-4 px-0 align-items-center overflow-auto" style={{height: "calc(100% - 91px)"}}>
                     <img className="img-fluid w-75 mb-4" src={logo} alt="Private Code" />
                     <ProductsTable />
                 </div>
